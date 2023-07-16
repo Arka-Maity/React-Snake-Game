48a2ec402e9d54508ed5820ddb55753bec1052c9
Arka Maity <129553640+Arka-Maity@users.noreply.github.com>
2023-07-16 14:16:24 +0530

    Update Food.js

--- src/components/Food.js
+++ src/components/Food.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 export default (props) => {
 
- 
+  const style = {
+    left: `${props.dot[0]}%`,
+    top: `${props.dot[1]}%`
+  }
 
   return (
-    <div className="snake-food"></div>
+    <div className="snake-food" style={style}></div>
   )
 }
