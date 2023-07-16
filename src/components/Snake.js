ec5770b8fc7837b9fa24f4c78254c99742464117
Arka Maity <129553640+Arka-Maity@users.noreply.github.com>
2023-07-16 14:15:35 +0530

    Update Snake.js

--- src/components/Snake.js
+++ src/components/Snake.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 export default (props) => {
-;
+  const ref = useRef();
 
   return (
     <div>
-          <div className="snake-dot"></div>
+      {props.snakeDots.map((dot, i) => {
+        const style = {
+          left: `${dot[0]}%`,
+          top: `${dot[1]}%`
+        }
+        return (
+          <div className="snake-dot" ref={ref} key={i} style={style}></div>
+        )
+      })}
     </div>
   )
 }
